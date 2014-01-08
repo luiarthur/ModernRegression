fb065165d5c65fab7d8c07566061eb9345775e4e
luiarthur <luiarthur@yahoo.com>
2014-01-07 17:14:21 -0700

    Conceptualized

--- Homework/1/Credit.R
+++ Homework/1/Credit.R
@@ -2,12 +2,13 @@
 #       Determining characteristics of a cardholder that lead to high balances
 #       Want: Moderate Balance, Avoid: High Balance, Don't Care about: Low Balance
 
+rm(list=ls())
 options("width"=120)
 credit <- read.table("Credit.csv",header=T,sep=',')
 credit <- credit[,-1]
 credit <- as.data.frame(credit)
-credit <- 
-
+credit$Cards <- paste(credit$Cards)
+credit$Cards[credit$Cards>="7"] <- "7+"
 #head(credit)
 #pairs(credit)
 #
@@ -28,9 +29,13 @@ credit <-
 #table(credit$Ethnicity)
 
 credit$Cards <- factor(credit$Cards)
-credit$Cards <- factor(credit$Gender)
-credit$Cards <- factor(credit$Student)
-credit$Cards <- factor(credit$Married)
-credit$Cards <- factor(credit$Ethnicity)
+credit$Gender <- factor(credit$Gender)
+credit$Student <- factor(credit$Student)
+credit$Married <- factor(credit$Married)
+credit$Ethnicity <- factor(credit$Ethnicity)
 
-mod <- lm(Balance ~ Income + Limit + Rating + Age + Cards + Gender + Student + Married + Ethnicity, data=credit)
+mod1 <- lm(Balance ~ Income + Limit + Rating + Age + Cards + Gender + Student + Married + Ethnicity, data=credit)
+mod2 <- lm(Balance ~ Income + Limit + Rating + Age + Cards + Student, data=credit)
+
+summary(mod1)
+summary(mod2)
