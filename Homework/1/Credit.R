d7c835a012a0c3433f64ad75c4daff40d315ce31
luiarthur <luiarthur@yahoo.com>
2014-01-14 17:37:54 -0700

    yes

--- Homework/1/Credit.R
+++ Homework/1/Credit.R
@@ -1,10 +1,12 @@
+# Read: P.203-214, 245-250
 # Goal: Predict the balance of cardholders BEFORE issuing card
 #       Determining characteristics of a cardholder that lead to high balances
 #       Want: Moderate Balance, Avoid: High Balance, Don't Care about: Low Balance
 
 rm(list=ls())
 options("width"=120)
-library(car) #vif
+library(car)    #vif
+library(leaps)  #regsubsets
 
 credit <- read.table("http://mheaton.byu.edu/Courses/Stat536/Case%20Studies/Credit/Data/Credit.csv",header=T,sep=",")
 credit <- credit[,-1]
@@ -30,9 +32,9 @@ credit <- as.data.frame(credit)
 #table(credit$Married)
 #table(credit$Ethnicity)
 
-trainSet <- sample(1:400,390)
+trainSet <- sample(1:400,300)
 testSet  <- setdiff(1:400,trainSet)
-  
+
 train <- credit[trainSet,]
 test  <- credit[testSet,]
 
@@ -41,23 +43,23 @@ train$Student <- factor(train$Student)
 train$Married <- factor(train$Married)
 train$Ethnicity <- factor(train$Ethnicity)
 
-lower.mod <- lm(Balance ~ 1, data=train)
-upper.mod <- lm(Balance ~ ., data=train)
-
-summary(upper.mod)
-
-forwardS <- step(lower.mod,scope=list(lower=lower.mod,upper=upper.mod),upper=upper.mod,direction="both")
-mod <- eval(forwardS$call)
-
-summary(mod)
-
-vif(mod)
-  # Limit  15.44
-  # Rating 15.47
-
 par(mfrow=c(2,1))
-hist(resid(mod)) #Residuals not symmetric, looks Gamma
-qqnorm(resid(mod))
+lower.mod <- lm(Balance ~ 1, data=train)
+#upper.mod <- lm(Balance ~ ., data=train)
+#
+#summary(upper.mod)
+#
+#forwardS <- step(lower.mod,scope=list(lower=lower.mod,upper=upper.mod),upper=upper.mod,direction="both")
+#mod <- eval(forwardS$call)
+#
+#summary(mod)
+#
+#vif(mod)
+#  # Limit  15.44
+#  # Rating 15.47
+#
+#hist(resid(mod)) #Residuals not symmetric, looks Gamma
+#qqnorm(resid(mod))
 # How do I get the Bias?
 # How do I get the MSE?
  
@@ -74,10 +76,39 @@ qqnorm(resid(mod2))
 par(mfrow=c(1,1))
 
 # Test the test set for mod2
-#X <- credit[testSet,]
-#x <- cbind(1, X$Rating, X$Income, X$Student, X$Limit, X$Cards, X$Age)
-#x[,4] <- x[,4] - 1
-#b <- mod$coefficients
-#
-#x %*% b
-#X
+#pos <- function(c,s){
+#  p <- 0
+#  while( (substr(s,p,p) != c) & (p < nchar(s) )){
+#    p <- p + 1
+#  }
+#  ifelse(substr(s,p,p)==c,p,0)
+#}
+
+swap <- function(s){
+  #p <- pos(":",s)
+  p <- regexpr(":",s)
+  if (p > 0 ){
+    a   <- substr(s,1,p-1)
+    b   <- substr(s,p+1,nchar(s))
+    ifelse(a<b, paste(a,b,sep=":"), paste(b,a,sep=":")) 
+  } else {
+    s
+  }
+}
+
+x <- model.matrix(Balance ~ .^2, data=test)
+b <- mod2$coef
+colnames(x) <- sapply(colnames(x),swap); colnames(x) <- unname(colnames(x))
+names(b) <- sapply(names(b),swap)
+
+x <- x[, names(b)]
+
+pred <- x %*% b
+MSE <- mean((test[,"Balance"]-pred)^2)
+
+#crazy <- regsubsets(Balance ~ .^2, data=train, nvmax=25, method="forward")
+#crazy.summ <- summary(crazy)
+#crazy.summ$rsq
+#coef(crazy,10)
+
+
