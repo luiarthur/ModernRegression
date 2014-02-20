42f65a8193c1bba6c4f68c433c6bca582333fc25
luiarthur <luiarthur@luiarthur-LIFEBOOK-MH330.(none)>
2014-02-20 07:28:30 -0700

    changed cars.R

--- Homework/3/cars.R
+++ Homework/3/cars.R
@@ -1,12 +1,28 @@
 rm(list=ls())
-options("width"=80)
 
 cars <- read.csv("Cars.csv",header=T)
-cars$cc[81] <- 1600
+cars$cc[81] <- 1600 # Because this was obviously a mistake
+cars <- cars[,-c(1,4,13)] # Cylinders is removed because they're all 4 Cylinders.
 
 library(gam)
 library(splines)
 
-smooth.spline
-head(cars)
+# Spline for miles and other variables are categorical
 
+  mod <- smooth.spline(cars$Miles,cars$Price,cv=T)
+  lambda <- mod$lambda
+
+  plot.smooth.spline <- function(){
+    plot(cars$Miles,cars$Price,col="pink",pch=20,
+         xlab="Price",ylab="Miles",
+         main="Price Vs. Miles")
+
+    lines(mod,lwd=3,col='gold')
+    legend("topright",legend="Smoothing Spline",
+           col="gold",lwd=3)
+  }
+  
+  plot.smooth.spline()
+
+# GAM:
+  # gam <- 
