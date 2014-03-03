2f16d8612a2c9aa6a97d8a29bfee879383433a6f
luiarthur <luiarthur@yahoo.com>
2014-03-03 12:52:56 -0700

    yes

--- Midterm/R/oz.R
+++ Midterm/R/oz.R
@@ -2,13 +2,12 @@ rm(list=ls())
 library(LatticeKrig)
 
 
-cmaq  <- read.csv("../Data/CMAQ.csv")       # 66960 4
-o3    <- read.csv("../Data/Ozone.csv")      #   800 6
-predL <- read.csv("../Data/PredLocs.csv")   #  2834 4
+cmaq  <- read.csv("../Data/CMAQ.csv")       # 66960 x 4
+o3    <- read.csv("../Data/Ozone.csv")      #   800 x 6
+predL <- read.csv("../Data/PredLocs.csv")   #  2834 x 4
 
 head(cmaq)
 head(o3)
 head(predL)
 
-D <- rdist(o3[,5:4],cmaq[,1:2]) # 800x67000 Matrix of Distances
-dim(D) 
+D <- rdist(o3[,5:4],cmaq[,1:2]) # 800 x 66960 
