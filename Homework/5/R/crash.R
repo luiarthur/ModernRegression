ba699adda7ed2016f9db37844bc9d7022e3d4387
Arthur Lui <luiarthur@yahoo.com>
2014-03-11 09:44:21 -0600

    yes

--- Homework/5/R/crash.R
+++ Homework/5/R/crash.R
@@ -11,10 +11,10 @@ mod.f <- glm(Fatal ~ ., data=crash, family=binomial("logit"))
 
 #mod.hour <- glm(Fatal ~ Hour, data=crash, family=binomial("logit"))
 #mod.A <- glm(Fatal ~ Age, data=crash, family=binomial("logit"))
-mod.G <- glm(Fatal ~ Sex, data=crash, family=binomial("logit"))           # ***
-mod.S <- glm(Fatal ~ Speed.Related, data=crash, family=binomial("logit")) # ***
-mod.L <- glm(Fatal ~ Speed.Limit, data=crash, family=binomial("logit"))   # ***
-mod.D <- glm(Fatal ~ Distracted, data=crash, family=binomial("logit"))    # ***
+mod.G  <- glm(Fatal ~ Sex, data=crash, family=binomial("logit"))           # ***
+mod.S  <- glm(Fatal ~ Speed.Related, data=crash, family=binomial("logit")) # ***
+mod.L  <- glm(Fatal ~ Speed.Limit, data=crash, family=binomial("logit"))   # ***
+mod.D  <- glm(Fatal ~ Distracted, data=crash, family=binomial("logit"))    # ***
 mod.Dg <- glm(Fatal ~ Drugs, data=crash, family=binomial("logit"))        # ***
 
 #mod.s <- step(mod.1,scope=list(lower=mod.1,upper=mod.f),data=crash,direction="both")
