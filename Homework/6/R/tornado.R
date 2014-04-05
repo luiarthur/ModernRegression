0799d13a0241afdfe322a68793b02dadbadad203
Arthur Lui <luiarthur@yahoo.com>
2014-04-05 08:49:09 -0600

    yes

--- Homework/6/R/tornado.R
+++ Homework/6/R/tornado.R
@@ -1,1 +1,11 @@
-yes
+rm(list=ls())
+
+# My Functions:
+  rmCol  <- function(M,cn) M[,-c(which(colnames(M)==cn))]
+  getCol <- function(M,cn) M[, c(which(colnames(M)==cn))]
+  
+# Clean Data:
+  # Beware of repeating observations
+  # Build model using F-scale and past data
+  dat <- read.csv("../Data/tornados.csv")
+  dat <-rmCol(dat,"Year")
