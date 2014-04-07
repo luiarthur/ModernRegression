597d5f06ae5ecf8164038b79532c0ce9abc3b554
luiarthur <luiarthur@yahoo.com>
2014-04-07 15:33:10 -0600

    yes

--- Homework/6/R/tornado.R
+++ Homework/6/R/tornado.R
@@ -1,15 +1,14 @@
+# Build model using F-scale and past data
 rm(list=ls())
 
 # My Functions:
-  rmCol  <- function(M,cn) M[,-c(which(colnames(M)==cn))]
-  getCol <- function(M,cn) M[, c(which(colnames(M)==cn))]
+  rmCol  <- function(M,cn) M[,-c(which(is.element(colnames(M),cn)))]
+  getCol <- function(M,cn) M[, c(which(is.element(colnames(M),cn)))]
   
 # Clean Data:
-  # Beware of repeating observations
-  # Build model using F-scale and past data
+  # Remove repeating observations
   dat <- read.csv("../Data/tornados.csv")
   dat <-rmCol(dat,"Year")
-  attach(dat)
   # dim(dat) # 957 x 17
 
   #"Number"     "Month"      "Day"        "Date"       "Time"      
@@ -17,9 +16,39 @@ rm(list=ls())
   #"CropLoss"   "StartLat"   "StartLon"   "EndLat"     "EndLon"    
   #"Length"     "Width"
 
-  plot(Number,Month)
-  repeat.ind <- NULL
-  for (i in 1:nrow(dat)) {
-    for (j in i:nrow(dat)) {
-    }
+  # What to do about repeating tornado Number?
+  dat <- subset(dat,!duplicated(dat$Number))
+  #attach(dat)
+
+  # Check for collinearity:
+
+####################################################################
+  # Random Forest:
+  library(randomForest)
+  set.seed(1)
+  pos <- regexpr(":",dat$Time)
+  dat$Time <- as.numeric(substr(dat$Time,1,pos-1))
+  trainI <- sample(1:nrow(dat),910)
+
+  library(foreach)
+  library(doMC)
+  registerDoMC(12)
+
+  one.it <- function(m) {
+    #small.dat <- rmCol(dat,c("Number","State","Date"))
+    small.dat <- rmCol(dat,c("Number","State","Date",
+                             "StartLat","StartLon","EndLat","EndLon"))
+    forest <- randomForest(as.factor(Fscale) ~ .,data=small.dat,
+                           subset=trainI,mtry=m,importance=T,ntree=10)
+    y.hat <- as.numeric(predict(forest,newdata=small.dat[-trainI,]))
+    acc <- mean(y.hat==dat$Fscale[-trainI])
+    
+    list("forest"=forest,"acc"=acc,"m"=m)
   }
+   
+  result <- foreach(i=1:12) %dopar% one.it(i)
+  (acc <- sapply(result,function(x) x$acc))
+
+  importance(forest)
+  varImpPlot(forest)
+
