1ad2f8e1e4e698b849d6438413bbfe2176ea6afc
luiarthur <luiarthur@yahoo.com>
2014-04-07 17:21:53 -0600

    yes

--- Homework/6/R/tornado.R
+++ Homework/6/R/tornado.R
@@ -28,27 +28,32 @@ rm(list=ls())
   set.seed(1)
   pos <- regexpr(":",dat$Time)
   dat$Time <- as.numeric(substr(dat$Time,1,pos-1))
-  trainI <- sample(1:nrow(dat),910)
+  trainI <- sample(1:nrow(dat),800)
 
   library(foreach)
   library(doMC)
-  registerDoMC(12)
+  registerDoMC(5)
 
   one.it <- function(m) {
     #small.dat <- rmCol(dat,c("Number","State","Date"))
-    small.dat <- rmCol(dat,c("Number","State","Date",
+    small.dat <- rmCol(dat,c("Number","State","Date","Month","Day","Time",
                              "StartLat","StartLon","EndLat","EndLon"))
+    #small.dat <- getCol(dat,c("Fscale","Injuries","Fatalities","Loss",
+    #                          "CropLoss","Length","Width"))
     forest <- randomForest(as.factor(Fscale) ~ .,data=small.dat,
-                           subset=trainI,mtry=m,importance=T,ntree=10)
+                           subset=trainI,mtry=m,importance=T,ntree=1000)
     y.hat <- as.numeric(predict(forest,newdata=small.dat[-trainI,]))
     acc <- mean(y.hat==dat$Fscale[-trainI])
     
-    list("forest"=forest,"acc"=acc,"m"=m)
+    list("forest"=forest,"acc"=acc,"m"=m,"true"=dat$Fscale[-trainI],"pred"=y.hat)
   }
    
-  result <- foreach(i=1:12) %dopar% one.it(i)
+  result <- foreach(i=1:5) %dopar% one.it(i)
+  forests <- lapply(result,function(x) x$forest)
   (acc <- sapply(result,function(x) x$acc))
-
-  importance(forest)
-  varImpPlot(forest)
+  
+  importance(forests[[1]])
+  varImpPlot(forests[[1]])
+  plot(forests[[1]])
+  forests[[1]]
 
