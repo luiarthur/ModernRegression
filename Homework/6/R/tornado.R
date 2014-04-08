cb630281cb2304008ada73bff0ce5abb80ff0fc3
luiarthur <luiarthur@yahoo.com>
2014-04-08 15:08:39 -0600

    yes

--- Homework/6/R/tornado.R
+++ Homework/6/R/tornado.R
@@ -7,9 +7,9 @@ rm(list=ls())
   
 # Clean Data:
   # Remove repeating observations
-  dat <- read.csv("../Data/tornados.csv")
-  dat <-rmCol(dat,"Year")
-  dat$Fscale <- as.factor(dat$Fscale)
+  Dat <- read.csv("../Data/tornados.csv")
+  Dat <-rmCol(Dat,"Year")
+  Dat$Fscale <- as.factor(Dat$Fscale)
   # dim(dat) # 957 x 17
 
   #"Number"     "Month"      "Day"        "Date"       "Time"      
@@ -18,7 +18,9 @@ rm(list=ls())
   #"Length"     "Width"
 
   # What to do about repeating tornado Number?
-  dat <- subset(dat,!duplicated(dat$Number))
+  #dat <- subset(Dat,!duplicated(Dat$Number))
+  dat <- Dat[-c(120,122,113,116,158,168,148,149,400,187,188,200,202,209,179,180,936),]
+
   #attach(dat)
 
   # Check for collinearity:
@@ -33,13 +35,15 @@ rm(list=ls())
 
   library(foreach)
   library(doMC)
-  registerDoMC(5)
+  registerDoMC(16)
 
   #small.dat <- rmCol(dat,c("Number","State","Date"))
-  small.dat <- rmCol(dat,c("Number","State","Date","Month","Day","Time",
-                           "StartLat","StartLon","EndLat","EndLon"))
+  #small.dat <- rmCol(dat,c("Number","State","Date","Month","Day","Time",
+  #                         "StartLat","StartLon","EndLat","EndLon"))
   #small.dat <- getCol(dat,c("Fscale","Injuries","Fatalities","Loss",
   #                          "CropLoss","Length","Width"))
+  small.dat <- getCol(dat,c("Fscale","Month","Day","Time","Injuries",
+                            "Fatalities","Loss","CropLoss","Length","Width"))
 
   one.it <- function(m) {
     forest <- randomForest(Fscale ~ .,data=small.dat,
@@ -50,12 +54,17 @@ rm(list=ls())
     list("forest"=forest,"err"=err,"m"=m,"true"=dat$Fscale[-trainI],"pred"=y.hat)
   }
    
-  result <- foreach(i=1:(ncol(small.dat)-1)) %dopar% one.it(i)
+  result <- foreach(i=1:(ncol(small.dat)-2)) %dopar% one.it(i)
   forests <- lapply(result,function(x) x$forest)
   (err.rate <- sapply(result,function(x) x$err))
+  (best.m <- which.min(err.rate))
   
-  importance(forests[[1]])
-  varImpPlot(forests[[1]])
-  plot(forests[[1]])
-  forests[[2]]
+  forest <- randomForest(Fscale ~ .,data=small.dat,mtry=best.m,importance=T)
+
+  #(confusion <- rmCol(forest$confusion,"class.error"))
+  #1 - diag(confusion) / apply(confusion,1,sum)
+  #importance(forests[[1]])
+  ##varImpPlot(forests[[1]])
+  #plot(forests[[1]])
+  #forests[[2]]
 
